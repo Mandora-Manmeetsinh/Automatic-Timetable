bff12ac7d88e99b3e34d7295b19049af2659fe55
Kevan Tamboli <kevantamboli@gmail.com>
2025-02-18 16:06:51 +0530

    Groundwork for the login and all

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const dotenv = require("dotenv")
 const connection = require('./config/dbConfig')
 const app = express()
+const router = require('./router/user.routes')
 
 dotenv.config() // Getting the secretive files and data
 connection() // Setting up the connection
@@ -9,6 +10,8 @@ connection() // Setting up the connection
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
+
+app.use('/user',router)
 
 app.listen(process.env.PORT, ()=>{
     console.log(`http://localhost:3000`)
