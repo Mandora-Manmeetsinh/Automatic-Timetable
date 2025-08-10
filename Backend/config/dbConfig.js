48a855c034ff209e86cc2849f97b4a216a0a4c06
Kevan Tamboli <kevantamboli@gmail.com>
2025-08-10 15:41:57 +0530

    Developed Schemas. Kevan has arrived

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
-const getConnection = ()=>{
-    const con = mongoose.connect(process.env.MONGO_URI)
-    .then((err)=>{
-        if(err){
-            console.log(err.toString())
-        }
-        console.log("db connected")
-    })
-}
+const mongoose = require("mongoose");
 
-module.exports = getConnection
+const con = mongoose
+  .connect("mongodb://localhost:27017/V2tabularDSA")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+module.exports = con;
+
+/**
+ mongoose.connect('mongodb://localhost:27017/tabularDSA', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+ */
--- Backend/models/division.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const divisionSchema = new mongoose.Schema({
-    code: { type: String, required: true },
-    name: { type: String, required: true },
-    // Add other division-specific fields as needed
-});
-
-module.exports = mongoose.model('Division', divisionSchema);
